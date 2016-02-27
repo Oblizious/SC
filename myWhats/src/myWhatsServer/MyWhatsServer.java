61a5c20707d7366794313ffb3994e8975100476c
Telmo Santos <fc44839@fc.ul.pt>
2016-02-27 17:21:28 +0000

    created server package

---
+++ myWhats/src/myWhatsServer/MyWhatsServer.java
@@ -0,0 +1,10 @@
+package myWhatsServer;
+
+public class MyWhatsServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ myWhats/src/myWhatsServer/MyWhatsServerSkeleton.java
@@ -0,0 +1,5 @@
+package myWhatsServer;
+
+public class MyWhatsServerSkeleton {
+
+}
