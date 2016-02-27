51980334de928c25df22622f349fd9ae458808fd
Telmo Santos <fc44839@fc.ul.pt>
2016-02-27 18:46:29 +0000

    stuff & stuff

--- myWhats/src/myWhatsServer/MyWhatsServerSkeleton.java
+++
@@ -1,5 +0,0 @@
-package myWhatsServer;
-
-public class MyWhatsServerSkeleton {
-
-}
--- myWhats/src/myWhatsServer/Persistence.java
+++ myWhats/src/myWhatsServer/Persistence.java
@@ -1,5 +1,69 @@
 package myWhatsServer;
 
-public class Persistence {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Persistence {
+	private static final Persistence INSTANCE = new Persistence();
+	private Map<String,User> users;
+	
+	private Persistence() {
+		try {
+			File usersFile = new File("/Data/users");
+			users = new HashMap<>();
+			
+			BufferedReader r = new BufferedReader(new FileReader(usersFile));
+			
+			String s;
+			while((s = r.readLine()) != null){
+				User u = getUser(s);
+				if(u == null){
+					System.out.println("O ficheiro de utilizadores está corrupto.");
+					r.close();
+					System.exit(0);
+				}					
+				users.put(u.getUsername(), u);
+			}				
+			r.close();
+		} catch (Exception e) {	e.printStackTrace(); }
+	}
+	
+	/*
+	 * Returna true se bem sucedido, false caso ocorra erro
+	 */
+	public boolean saveMessage(String username, Calendar timestamp, String message){
+		String filename = timestamp.toString();
+		File f = new File("/Data/username/"+filename);
+		
+		try {
+			BufferedWriter w = new BufferedWriter(new FileWriter(f));
+			w.write(message+"\n");
+			w.write(timestamp.toString());
+			w.close();
+		} catch (IOException e) {e.printStackTrace(); return false;}
+		
+		return true;
+	}
+
+	/*
+	 * Retorna o utilizador representado pela string s sob o formato user:password
+	 * Return null caso o formato nao esteja em conformidade
+	 */
+	private User getUser(String s) {
+		String [] info = s.split(":");
+		if(info.length != 2)
+			return null;
+		return new User(info[0], info[1]);
+	}
+	
+	public static Persistence getInstance() {
+        return INSTANCE;
+    }	
 }
